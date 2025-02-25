4c7eb2dd30af110d810ff74c4ca7b7cd6dd9b77f
William Crane <wcran7@gmail.com>
2025-02-26 07:55:12 +1000

    FIX: Centerd the navbar by adding full width to header container

--- portfolio/portfolio/src/sections/Header.tsx
+++ portfolio/portfolio/src/sections/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
-  <div className="flex justify-center items-center fixed top-3">
-    <nav className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
+  <div className="flex justify-center items-center fixed top-3 w-full px-4">
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
       <a href="#" className="nav-item" >Home</a>
       <a href="#" className="nav-item">Projects</a>
       <a href="#" className="nav-item">About</a>
