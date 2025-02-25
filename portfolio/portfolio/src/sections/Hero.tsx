cae9d4125b352c637200b44b8bc7507420c96473
William Crane <wcran7@gmail.com>
2025-02-26 07:51:57 +1000

    Hero Section: Finalised initial styling for mobile hero section

--- portfolio/portfolio/src/sections/Hero.tsx
+++ portfolio/portfolio/src/sections/Hero.tsx
@@ -8,24 +8,24 @@ export const HeroSection = () => {
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center">
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
               <div className="bg-green-500 size-2.5 rounded-full"></div>
               <div className="text-sm font-medium">Available for new projects</div>
           </div>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
+        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+        <p className="mt-4 text-center text-white/60">
           I specialise in transforming designs into functional, high-performing
           web applications. Lets discuss your next project.
         </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className="flex flex-col items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
           </button>
-          <button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span>Let's Connect</span>
+            <span className="font-semibold">Let's Connect</span>
           </button>
         </div>
       </div>
