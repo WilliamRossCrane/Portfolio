9d29bacb5f662692faf283a067134fe7e4dd5e76
William Crane <wcran7@gmail.com>
2025-02-26 07:42:55 +1000

    Hero Section: Initial Styling for memoji image and text

--- portfolio/portfolio/src/sections/Hero.tsx
+++ portfolio/portfolio/src/sections/Hero.tsx
@@ -4,12 +4,14 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="py-32">
       <div className="container">
-        <Image src={memojiImage} alt="Person peeking from behind laptop" />
-        <div>
-            <div></div>
-            <div>Available for new projects</div>
+        <div className="flex flex-col items-center">
+          <Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center">
+              <div className="bg-green-500 size-2.5 rounded-full"></div>
+              <div className="text-sm font-medium">Available for new projects</div>
+          </div>
         </div>
         <h1>Building Exceptional User Experiences</h1>
         <p>
