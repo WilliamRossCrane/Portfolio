d5d27bf07eeb73672148830ac0664ca0b8e97e36
William Crane <wcran7@gmail.com>
2025-02-27 07:55:47 +1000

    Hero Section: Added in Responsive designs for md and lg views

--- portfolio/portfolio/src/sections/Hero.tsx
+++ portfolio/portfolio/src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
@@ -13,12 +13,14 @@ export const HeroSection = () => {
               <div className="text-sm font-medium">Available for new projects</div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
-        <p className="mt-4 text-center text-white/60">
+        <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">
           I specialise in transforming designs into functional, high-performing
           web applications. Lets discuss your next project.
         </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
