186b474807d3790fb7208a1ff427f630d934cad6
William Crane <wcran7@gmail.com>
2025-03-06 20:22:00 +1000

    Projects Section: Mobile View style finalised.

--- portfolio/portfolio/src/sections/Projects.tsx
+++ portfolio/portfolio/src/sections/Projects.tsx
@@ -4,6 +4,7 @@ import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from '@/assets/images/grain.jpg';
 
 const portfolioProjects = [
   {
@@ -45,23 +46,30 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>
+  return
+  
+    <section className="pb-16">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">Real-world Results</p>
         </div>
         <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
         <p className="text-center text-white/60 mt-4">See how I transformed conepts into engaging digital experience.</p>
-        <div className="flex flex-col mt-10">
+        <div className="flex flex-col gap-20 mt-10">
           {portfolioProjects.map((project) => (
-            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8">
-              <div className="flex">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
-                </div>
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none">
+            <div className="absolute inset-0 -z-10 opacity-5" 
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+            ></div>
+              
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+              <span>{project.company}</span>
+              <span>&bull;</span>
+              <span>{project.year}</span>
               </div>
+              
               <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
               <hr className="border-t-2 border-white/5 mt-4"/>
               <ul className="flex flex-col gap-4 mt-4">
@@ -74,7 +82,7 @@ export const ProjectsSection = () => {
               </ul>
               <a href={project.link}>
                 <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                  <span>View Live Site</span>
+                  <span>Visit Live Site</span>
                   <ArrowUpRightIcon className="size-4"/>
                 </button>
               </a>
@@ -83,5 +91,5 @@ export const ProjectsSection = () => {
           ))}
         </div>
       </div>
-    </div>;
+    </section>;
 };
