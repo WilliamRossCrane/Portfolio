b6f82dfc02746d7c1ac2436e53a539de8c16a1e1
William Crane <wcran7@gmail.com>
2025-03-06 21:00:42 +1000

    Project Section: Desktop/LG Version complete, global container styling added for lg view

--- portfolio/portfolio/src/sections/Projects.tsx
+++ portfolio/portfolio/src/sections/Projects.tsx
@@ -47,22 +47,25 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return(
-    <section className="pb-16">
+    <section className="pb-16 lg:py24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">Real-world Results</p>
         </div>
         <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
-        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">See how I transformed conepts into engaging digital experience.</p>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed conepts into engaging digital experience.</p>
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none">
+            
             <div className="absolute inset-0 -z-10 opacity-5" 
             style={{
               backgroundImage: `url(${grainImage.src})`,
             }}
             ></div>
-              
+
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
               <span>{project.company}</span>
               <span>&bull;</span>
@@ -85,7 +88,12 @@ export const ProjectsSection = () => {
                   <ArrowUpRightIcon className="size-4"/>
                 </button>
               </a>
-              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0"/>
+              </div>
+
+              <div className="relative">
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
+              </div>
+              </div>
             </div>  
           ))}
         </div>
