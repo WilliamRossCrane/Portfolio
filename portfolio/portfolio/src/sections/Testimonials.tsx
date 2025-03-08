7e5d7040e86971409b73f4a87df834a60bd66291
William Crane <wcran7@gmail.com>
2025-03-08 11:05:30 +1000

    Testimonial Section: mobile view is complete now ready for tablet and desktop view adjustments

--- portfolio/portfolio/src/sections/Testimonials.tsx
+++ portfolio/portfolio/src/sections/Testimonials.tsx
@@ -43,20 +43,30 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-  <div className="py-16">
-  <div>
-    <SectionHeader eyebrow="Happy Clients" title="What Clients Say about" description="Don't just take my word for it. See what my clients have to say about my work." />
-    <div>
-    {testimonials.map((testimonial) => (
-      <Card key={testimonial.name}>
-          <Image src={testimonial.avatar} alt={testimonial.name} />
-          <div>{testimonial.name}</div>
-          <div>{testimonial.position}</div>
-          <p>{testimonial.text}</p>
-      </Card>
-      ))}
+    <div className="py-16">
+      <SectionHeader
+        eyebrow="Happy Clients"
+        title="What Clients Say"
+        description="Don't just take my word for it. See what my clients have to say about my work."
+      />
+
+      <div className="mt-16 flex gap-8 overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        {testimonials.map((testimonial) => (
+          <Card key={testimonial.name} className="max-w-xs flex-none">
+            <div className="flex gap-4 items-center">
+              <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+              </div>
+              <div>
+                <div className="font-semibold">{testimonial.name}</div>
+                <div className="text-sm text-white/40">{testimonial.position}</div>
+              </div>
+            </div>
+            <p className="mt-4 text-sm">{testimonial.text}</p>
+          </Card>
+        ))}
+      </div>
     </div>
-    </div>
-  </div>
   );
 };
+
