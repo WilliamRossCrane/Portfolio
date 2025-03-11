372a68938fc4b3f62c324e608d2f25fd1b8652ec
William Crane <wcran7@gmail.com>
2025-03-11 21:33:35 +1000

    About Section: Inititial structure completed, ready to create global class for the Icon styling.

--- portfolio/portfolio/src/sections/About.tsx
+++ portfolio/portfolio/src/sections/About.tsx
@@ -1,3 +1,35 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from '@/assets/icons/star.svg';
+import Image from "next/image";
+import bookImage from '@/assets/images/book-cover.png';
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="pb-96">
+      <SectionHeader
+        eyebrow="About Me" 
+        title="A Glimpse Into My World" 
+        description="Learn more about who I am, why I do, and what inspires me"
+      />
+
+      <div>
+        <Card>
+          <StarIcon />
+          <h3>My Reads</h3>
+          <p>Explore the books shaping my perspectives.</p>
+        </Card>
+      </div>
+      <Image src={bookImage} alt="Book cover" />
+
+      <Card>
+          <StarIcon />
+          <h3>My Reads</h3>
+          <p>Explore the books shaping my perspectives.</p>
+        </Card>
+      </div>
+      <Image src={bookImage} alt="Book cover" />
+
+    </div>
+  );
 };
