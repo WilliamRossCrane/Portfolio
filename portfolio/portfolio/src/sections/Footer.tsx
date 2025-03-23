0ad3678634cb7e934f1e9d78c57c25da17958fcd
William Crane <wcran7@gmail.com>
2025-03-23 19:44:13 +1000

    Footer Section: Layout complete, ready for styling

--- portfolio/portfolio/src/sections/Footer.tsx
+++ portfolio/portfolio/src/sections/Footer.tsx
@@ -1,3 +1,41 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+
+const footerLinks = [
+  {
+    title:'Youtube',
+    href: '#',
+  },
+  {
+    title:'Twitter',
+    href: '#',
+  },
+  {
+    title:'Instagram',
+    href: '#',
+  },
+  {
+    title:'LinkedIn',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return 
+  <footer>
+    <div className="Container" >
+      <div className="border-t border-white/15">
+        <div>&copy: 2024. All rights reserved</div>
+        <div>
+          <nav>
+            {footerLinks.map(link => (
+              <a href='#' key={link.title}>
+                <span>{link.title}</span>
+                <ArrowUpRightIcon />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>;
 };
