0c7a86396e52fc7de9d5316353b7deb113e5fba0
William Crane <wcran7@gmail.com>
2025-03-25 08:17:31 +1000

    ANIMATION: Hero Section, available pulse animation added in using the tailwind config and hero file

--- portfolio/portfolio/tailwind.config.ts
+++ portfolio/portfolio/tailwind.config.ts
@@ -1,32 +1,47 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    screens: {
-      sm: "375px",
-      md: "768px",
-      lg: "1200px",
-    },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
-        md: "2rem",
-      },
-    },
-    extend: {
-      fontFamily: {
-        sans: 'var(--font-sans)',
-        serif: 'var(--font-serif)',
-      }
+content: [
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+],
+theme: {
+  screens: {
+    sm: "375px",
+    md: "768px",
+    lg: "1200px",
+  },
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: "1rem",
+      md: "2rem",
     },
   },
-  plugins: [],
+  extend: {
+    fontFamily: {
+      sans: 'var(--font-sans)',
+      serif: 'var(--font-serif)',
+    },
+    animation: {
+      'ping-large': 'ping-large 1.5s ease-out infinite'
+    },
+    keyframes: {
+      'ping-large': {
+        '0%': { 
+          transform: 'scale(1)', 
+          opacity: '1' 
+        },
+        '100%': {
+          transform: 'scale(3)',
+          opacity: '0',
+        }
+      }
+    }
+  },
+},
+plugins: [],
 };
 export default config;
