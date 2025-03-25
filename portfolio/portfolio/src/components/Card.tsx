75c756bfda5cc8aa93e3e27f9a99d8551c3201ab
William Crane <wcran7@gmail.com>
2025-03-25 20:25:14 +1000

    ANIMATION: Projects Section, sticky affect added for cards

--- portfolio/portfolio/src/components/Card.tsx
+++ portfolio/portfolio/src/components/Card.tsx
@@ -1,11 +1,11 @@
 import grainImage from "@/assets/images/grain.jpg";
 import { twMerge } from "tailwind-merge";
 import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef } from "react";
 
-export const Card = ({ className, children }: 
-    PropsWithChildren<{ className?: string }>) => {
+export const Card = ({ className, children, ...other }: ComponentPropsWithoutRef<"div">) => {
     return (
-        <div className={twMerge("bg-gray-800 text-white rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}>
+        <div className={twMerge("bg-gray-800 text-white rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}{...other}>
             
             {/* Background Image Overlay */}
             <div 
